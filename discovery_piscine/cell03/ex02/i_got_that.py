6ceebc7ae97c6d8afcf20a695a3fe56720aa7d16
I3eaIVI <i3eaivi6906@gmail.com>
2026-09-23 21:13:44 +0700

    update3

---
+++ discovery_piscine/cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+text = input("What you gotta say? : ")
+while True:
+    text = input("I got that! Anything else? : ")
+    if text == "STOP":
+        break
---
+++ discovery_piscine/cell03/ex03/advanced_mult.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    if len(sys.argv) > 1:
+        print("none")
+    else:
+        i = 0
+        while i <= 10:
+            j = 0
+            print(f"Table de {i}: ", end="")
+            while j <= 10:
+                print(f"{i * j}", end=" ")
+                j += 1
+            print()
+            i+=1
+main()
